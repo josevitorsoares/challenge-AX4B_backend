e858264f31fcf731c1476539599a6030375eacb7
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-12-11 11:01:38 -0300

    FEAT: Add error handling

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,10 @@
 import fastify from "fastify";
 import cors from "@fastify/cors";
+import { ZodError } from "zod";
+
 import { appRoutes } from "./http/routes";
+
+import { env } from "./env";
 
 export const app = fastify();
 
@@ -12,4 +16,19 @@ app.register(cors, {
     allowedHeaders: [
         "Content-Type"
     ]
+});
+
+app.setErrorHandler((error, _, reply)=> {
+    if (error instanceof ZodError) {
+        return reply.status(400).send({
+            message: "Validation Error",
+            error: error.format()
+        });
+    }
+    if (env.NODE_ENV === "development") {
+        console.error(error);
+    } 
+    return reply.status(500).send({
+        error: "Internal Server Error"
+    });
 });
