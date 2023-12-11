39b58f50190f1666a4bd5ad0ee7f77fa5f80eaf1
José Vitor Soares <josevitorgoncalvessoares@gmail.com>
2023-12-11 10:34:54 -0300

    FEAT: Add routes and cors configs

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
 import fastify from "fastify";
+import cors from "@fastify/cors";
+import { appRoutes } from "./http/routes";
 
 export const app = fastify();
+
+app.register(appRoutes);
+
+app.register(cors, {
+    origin: "*",
+    methods: ["GET"],
+    allowedHeaders: [
+        "Content-Type"
+    ]
+});
